4461c82e6b28b338ddbaaed9751870777a25a5bb
Hao-Liang Wen <luisleo526@yahoo.com>
2022-08-08 11:15:25 +0800

    debug

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ if __name__ == '__main__':
     p = GraphManager(args.n, args.t)
     data = p.get_all_data()
 
-    with pd.ExcelWriter(f"n={args.n}.xlsx") as writer:
+    with pd.ExcelWriter(f"n=4.xlsx") as writer:
         ranks = {'rank': []}
         for d in data:
-            pd.DataFrame(data=data[d].full, index=data[d].rows, columns=data[d].columns).to_excel(writer, sheet_name=d)
+            pd.DataFrame(data=data[d].full.transpose(), index=data[d].rows, columns=data[d].columns).to_excel(writer, sheet_name=d)
             ranks['rank'].append(data[d].rank)
         pd.DataFrame(data=ranks, index=list(data.keys())).to_excel(writer, sheet_name='ranks')
 
-    with open(f"n={args.n}-graphs.txt", "w") as f:
+    with open(f"n=4-graphs.txt", "w") as f:
         for i in range(p.maxi + 1):
             graphs = getattr(p, chr(ord('A') + i))
             for prefix, gs in [[graphs.name, graphs.o], [graphs.name + 'N', graphs.no]]:
                 for cnt, g in enumerate(gs, 1):
-                    f.write(f"{prefix + str(cnt)}: {g.sG.edges}")
+                    f.write(f"{prefix + str(cnt)}: {g.sG.edges}\n")
