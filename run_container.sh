22f4559066b42f60e372838f4ea36f7fa19f2ea4
Hao-Liang Wen <luisleo526@yahoo.com>
2022-08-15 16:27:51 +0800

    debug

--- run_container.sh
+++ run_container.sh
@@ -1,12 +1,10 @@
-if [ ! "$(docker ps -q -f name=graph)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=graph)" ]; then
-            if [[ "$(docker images -q graph:latest 2> /dev/null)" == "" ]]; then
-                echo "FROM python:3.11.0rc1-bullseye" >> Dockerfile
-                echo "WORKDIR /app" >> Dockerfile
-                echo "RUN pip3 install openpyxl munch numpy pandas sympy" >> Dockerfile
-                docker build -t graph .
-            fi
-            docker run --name graph -itd -v $(pwd)/src:/app graph
+if [ ! $( docker ps -a | grep testContainer | wc -l ) -gt 0 ]; then
+    if [[ "$(docker images -q graph:latest 2> /dev/null)" == "" ]]; then
+        echo "FROM python:3.11.0rc1-bullseye" >> Dockerfile
+        echo "WORKDIR /app" >> Dockerfile
+        echo "RUN pip3 install openpyxl munch numpy pandas sympy" >> Dockerfile
+        docker build -t graph .
     fi
+    docker run --name graph -itd -v $(pwd)/src:/app graph
 fi
 docker exec graph /bin/bash -c "time python3 main.py -n $1 -t $2"
