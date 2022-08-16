d6469ddc38653d4435d2dc9fd88d070e9460493d
Hao-Liang Wen <20812494+luisleo526@users.noreply.github.com>
2022-08-16 09:28:26 +0800

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,10 +1,10 @@
 if [ ! $( docker ps -a | grep graph | wc -l ) -gt 0 ]; then
-    if [[ "$(docker images -q graph:latest 2> /dev/null)" == "" ]]; then
-        echo "FROM python:3.11.0rc1-bullseye" >> Dockerfile
-        echo "WORKDIR /app" >> Dockerfile
-        echo "RUN pip3 install openpyxl munch numpy pandas sympy" >> Dockerfile
-        docker build -t graph .
-    fi
-    docker run --name graph -itd -v $(pwd)/src:/app graph
+    #if [[ "$(docker images -q graph:latest 2> /dev/null)" == "" ]]; then
+    #    echo "FROM python:3.11.0rc1-bullseye" >> Dockerfile
+    #    echo "WORKDIR /app" >> Dockerfile
+    #    echo "RUN pip3 install openpyxl munch numpy pandas sympy" >> Dockerfile
+    #    docker build -t graph .
+    #fi
+    docker run --name graph -itd -v $(pwd)/src:/app luisleo52655/python3.11:1.0
 fi
 docker exec graph /bin/bash -c "time python3 main.py $@"
