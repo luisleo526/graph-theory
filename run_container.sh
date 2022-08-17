2e025e8555aadf36cfcbb622bd9e9dcfed84e6d6
leo <luisleo526@yahoo.com>
2022-08-18 02:12:01 +0800

    debug

--- run_container.sh
+++ run_container.sh
@@ -5,9 +5,10 @@ if [ ! $( docker ps -a | grep graph | wc -l ) -gt 0 ]; then
     #    echo "RUN pip3 install openpyxl munch numpy pandas sympy" >> Dockerfile
     #    docker build -t graph .
     #fi
-    docker run --name graph -itd -v $(pwd)/src:/app luisleo52655/python3.11 git pull origin master
+    docker run --name graph -itd -v $(pwd)/src:/app luisleo52655/python3.11
 fi
 if ! docker ps --format '{{.Names}}' | grep -w graph &> /dev/null; then
     docker start graph
 fi
+git pull origin master
 /bin/bash -c "docker exec graph /bin/bash -c 'time python3 -u main.py $@'" 2>&1 | tee log
