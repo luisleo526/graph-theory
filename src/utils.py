70b4610b7d0ce1e7bd4acf83634908860cadfac0
luis <luisleo526@yahoo.com>
2022-08-21 03:43:24 +0800

    debug

--- src/utils.py
+++ src/utils.py
@@ -16,48 +16,6 @@ from sympy import symbols, poly
 from sympy.matrices import Matrix
 
 
-def merge(*args):
-    left, right = args[0] if len(args) == 1 else args
-    left_length, right_length = len(left), len(right)
-    left_index, right_index = 0, 0
-    merged = []
-    while left_index < left_length and right_index < right_length:
-        if left[left_index] <= right[right_index]:
-            merged.append(left[left_index])
-            left_index += 1
-        else:
-            merged.append(right[right_index])
-            right_index += 1
-    if left_index == left_length:
-        merged.extend(right[right_index:])
-    else:
-        merged.extend(left[left_index:])
-    return merged
-
-
-def merge_sort(data):
-    length = len(data)
-    if length <= 1:
-        return data
-    middle = int(length / 2)
-    left = merge_sort(data[:middle])
-    right = merge_sort(data[middle:])
-    return merge(left, right)
-
-
-def merge_sort_parallel(data):
-    processes = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(processes=processes)
-    size = int(math.ceil(float(len(data)) / processes))
-    data = [data[i * size:(i + 1) * size] for i in range(processes)]
-    data = pool.map(merge_sort, data)
-    while len(data) > 1:
-        extra = data.pop() if len(data) % 2 == 1 else None
-        data = [(data[i], data[i + 1]) for i in range(0, len(data), 2)]
-        data = pool.map(merge, data) + ([extra] if extra else [])
-    return data[0]
-
-
 @njit(parallel=True)
 def mat_mult(A, B):
     assert A.shape[1] == B.shape[0]
@@ -69,7 +27,29 @@ def mat_mult(A, B):
     return res
 
 
-def compute_invar(adj):
+def hash_invar_set(adj):
+    invar_poly = []
+    invar_poly.append(Matrix(adj).charpoly(symbols('x')).as_expr())
+    for i in range(adj.shape[0]):
+        _adj = np.delete(np.delete(adj, i, 0), i, 1)
+        invar_poly.append(Matrix(_adj).charpoly(symbols('x')).as_expr())
+    invar_coeff = []
+    for expr in invar_poly:
+        invar_coeff.append(poly(expr).all_coeffs())
+    invar = sorted(set([tuple(x) for x in invar_coeff]))
+
+    m = hashlib.md5()
+    for p in invar:
+        msg = ""
+        for x in p:
+            msg += f"{x}"
+        m.update(msg.encode())
+    invar = m.hexdigest()
+
+    return invar
+
+
+def hash_invar(adj):
     invar_poly = []
     invar_poly.append(Matrix(adj).charpoly(symbols('x')).as_expr())
     for i in range(adj.shape[0]):
@@ -110,7 +90,7 @@ def find_unbind_number(adj):
                     expand_adj = np.copy(_expand_adj)
                     for loc, index in [[a, list(l1)], [b, l2]]:
                         expand_adj[loc, index] = expand_adj[index, loc] = 1
-                    unique[compute_invar(expand_adj)].append(1)
+                    unique[hash_invar(expand_adj)].append(1)
     return len(list(unique.keys()))
 
 
